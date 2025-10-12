0fa8a06763a7d320141c2ebbf238672c8037ed29
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-10-12 15:15:45 +0300

    fix guard

--- src/modules/auth/guards/auth.guard.js
+++ src/modules/auth/guards/auth.guard.js
@@ -2,7 +2,7 @@ import { AppError, ErrorCode } from '#common/app-error/app-error.js';
 import { AUTH_SESSION_COOKIE_NAME } from '../consts/index.js';
 
 export function authGuard(sessionStore) {
-  async function check(req, reply, done) {
+  async function check(req) {
     const sessionId = req.cookies[AUTH_SESSION_COOKIE_NAME];
     if (!sessionId) {
       throw new AppError(ErrorCode.INVALID_CREDENTIALS, 'Unauthorized');
@@ -15,7 +15,6 @@ export function authGuard(sessionStore) {
     }
 
     req.session = { userId: Number(session.userId), sessionId };
-    done();
   }
 
   return { check };
